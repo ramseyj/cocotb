28afdb189875623fec9bd81ba8d6652db2be756f
Jeff Ramsey <jramsey@rtlogic.com>
2014-09-12 11:05:02 -0600

    Updated make script file for cygwin.

--- make.bat
+++
@@ -1,2 +0,0 @@
-export PATH=/cygdrive/c/MinGW/bin/:/usr/local/lib/:/cygdrive/c/Python27/
-make SIM=aldec GPI_IMPL=vhpi ALDEC_PATH=/cygdrive/c/Aldec/ACTIVE~1.3
---
+++ make.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/bash
+PATH=/cygdrive/c/MinGW/bin/:/usr/local/lib/:/cygdrive/c/Python27/:$PATH
+make SIM=aldec-ahdl GPI_IMPL=vhpi ALDEC_PATH=/cygdrive/c/Aldec/ACTIVE~1.3
